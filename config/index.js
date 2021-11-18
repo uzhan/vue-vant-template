8eca136876e7952ba0e2f6c6b3e2a21d51db3cc9
BaiHuaYang <baihuayang@uphicoo.com>
2021-11-19 00:54:01 +0800

    chore: babel plugin html

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   // 项目名称
   title: '',
   // mock模式代理地址,为了方便演示，这里使用了fastmock线上服务，建议使用yapi,可以搭建私服， TODO: 按需修改
-  mock: '',
+  mock: 'http://127.0.0.1',
   // 开发模式代理地址 TODO: 按需修改
-  proxy: ''
+  proxy: 'http://127.0.0.1'
 }
