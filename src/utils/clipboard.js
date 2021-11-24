ea572b8308d40fbaa6ffd723f9eaa524d9a8dedf
BaiHuaYang <baihuayang@uphicoo.com>
2021-11-24 16:56:02 +0800

    feature: 增加clipboard一键复制

---
+++ src/utils/clipboard.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Clipboard from 'clipboard'
+
+function clipboardSuccess() {
+  Vue.prototype.$message({
+    message: '复制成功',
+    type: 'success',
+    duration: 1500
+  })
+}
+
+function clipboardError() {
+  Vue.prototype.$message({
+    message: '复制失败',
+    type: 'error'
+  })
+}
+
+/**
+ * @description 一键复制
+ * @author BaiHuaYang
+ * @date 24/11/2021
+ * @export
+ * @param {*} event 当前元素
+ * @param {*} text 要复制的内容
+ */
+export default function handleClipboard(event, text) {
+  const clipboard = new Clipboard(event.target, {
+    text: () => text
+  })
+  clipboard.on('success', () => {
+    clipboardSuccess()
+    clipboard.off('error')
+    clipboard.off('success')
+    clipboard.destroy()
+  })
+  clipboard.on('error', () => {
+    clipboardError()
+    clipboard.off('error')
+    clipboard.off('success')
+    clipboard.destroy()
+  })
+  clipboard.onClick(event)
+}
