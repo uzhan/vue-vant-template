c484cca79d9d343dff3196cd51d41d02e4f2c9d3
BaiHuaYang <baihuayang@uphicoo.com>
2021-11-25 15:09:15 +0800

    chore: app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div id="app">
+  <div id="root">
     <transition name="fade-transform" mode="out-in">
       <router-view />
     </transition>
   </div>
 </template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="less">
-#app {
+#root {
   height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
