02764f57cd9aff91cfee0ab406397455da3d6641
BaiHuaYang <baihuayang@uphicoo.com>
2021-11-25 14:42:46 +0800

    chore: autoprefixer&postcss-pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,9 @@ module.exports = {
       browsers: ['Android >= 4.0', 'iOS >= 7']
     },
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      rootValue({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
       propList: ['*']
     }
   }
