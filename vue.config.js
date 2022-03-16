82ce687902f6d01969d5620f2d65039902c032c4
BaiHuaYang <baihuayang@uphicoo.com>
2022-03-16 11:34:48 +0800

    fix: html title

--- vue.config.js
+++ vue.config.js
@@ -91,9 +91,6 @@ module.exports = {
   },
 
   chainWebpack: (config) => {
-    config.plugins.delete('preload') // TODO: need test
-    config.plugins.delete('prefetch') // TODO: need test
-
     // 别名 alias
     config.resolve.alias.set('@', resolve('src')).set('assets', resolve('src/assets')).set('api', resolve('src/api')).set('views', resolve('src/views')).set('components', resolve('src/components'))
 
