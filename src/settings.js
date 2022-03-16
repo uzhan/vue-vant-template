31f6e7f9906ec89712f4261e8b9783ffa81064b4
BaiHuaYang <baihuayang@uphicoo.com>
2022-03-16 11:42:12 +0800

    fix: $cdn default setting

--- src/settings.js
+++ src/settings.js
@@ -4,5 +4,7 @@ module.exports = {
   // mock模式代理地址,为了方便演示，这里使用了fastmock线上服务，建议使用yapi,可以搭建私服， TODO: 按需修改
   mock: 'http://127.0.0.1',
   // 开发模式代理地址 TODO: 按需修改
-  proxy: 'http://127.0.0.1'
+  proxy: 'http://127.0.0.1',
+  /** 向全局sass样式传入共享的全局变量, $src可以配置图片cdn前缀 */
+  $cdn: '/'
 }
