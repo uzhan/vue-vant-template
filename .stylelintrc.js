dea2806b9f6611d9c0dc3f2642e6a61f0b4c3b79
BaiHuaYang <baihuayang@uphicoo.com>
2023-02-19 16:15:42 +0800

    chore: init template

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: 'stylelint-config-ali',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-less',
+    'stylelint-config-standard-vue',
+  ],
+  plugins: ['stylelint-order'],
+  rules: {
+    // 校验规则略
+  },
 };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,6 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
@@ -28,9 +27,10 @@ const count = ref(0);
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <i>1</i>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 .read-the-docs {
   color: #888;
 }
