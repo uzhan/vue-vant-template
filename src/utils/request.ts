a590e15001e3a3ed83f0b7bfa7ab89f67a55b273
BaiHuaYang <baihuayang@uphicoo.com>
2024-09-13 17:35:15 +0800

    chore: 升级项目约束规范使用阿里的f2elint

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
-import { Toast } from 'vant'
+import axios from 'axios';
+import { showFailToast } from 'vant';
 
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API
+  baseURL: import.meta.env.VITE_APP_BASE_API,
 })
 
 // Request interceptors
@@ -10,12 +10,9 @@ service.interceptors.request.use(
   (config) => {
     // Add X-Access-Token header to every request, you can add other custom headers here
     const token = sessionStorage.getItem('token') || ''
-    const card_id = localStorage.getItem('card_id')
+
     if (token) {
       Object.assign(config.headers, { Authorization: token })
-    }
-    if (card_id) {
-      config.headers['card-id'] = card_id
     }
     return config
   },
@@ -32,7 +29,7 @@ service.interceptors.response.use(
       if (res.code === '100000') {
         sessionStorage.removeItem('token')
       } else {
-        res.message && Toast.fail(res.message)
+        res.message && showFailToast(res.message)
       }
       return Promise.reject(res.code)
     } else {
@@ -40,7 +37,7 @@ service.interceptors.response.use(
     }
   },
   (error) => {
-    Toast.fail('网络故障，请稍后再试！')
+    showFailToast('网络故障，请稍后再试！')
     return Promise.reject(error)
   }
 )
