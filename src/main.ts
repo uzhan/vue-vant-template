2def77b0a2a8919a18169d6bad0465893de9c732
BaiHuaYang <baihuayang@uphicoo.com>
2025-12-16 16:41:54 +0800

    feat: 增加支持tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia'
 import router from '@/router';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 import App from './App.vue';
 
-import 'vant/es/toast/style';
 import 'vant/es/dialog/style';
-import 'vant/es/notify/style';
 import 'vant/es/image-preview/style';
+import 'vant/es/notify/style';
+import 'vant/es/toast/style';
+
+// 导入 Tailwind CSS
+import '@/assets/tailwind.css';
 
 // 创建 Vue 实例
 const app = createApp(App);
 
 // 创建 Pinia 实例
-const pinia = createPinia()
+const pinia = createPinia();
 
 app.use(router).use(pinia).mount('#root');
