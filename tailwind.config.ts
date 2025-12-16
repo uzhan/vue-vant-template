4b1fb7a893f54546abe7d054afab245d5a5fdfb7
BaiHuaYang <baihuayang@uphicoo.com>
2025-12-16 17:32:49 +0800

    chore: 升级vite7

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false, // 禁用 Tailwind 的基础样式重置，避免与 Vant 冲突
+  },
+} satisfies Config;
